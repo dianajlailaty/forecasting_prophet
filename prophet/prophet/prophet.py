b5eeedf16ca055620283798d8e3d71494ad34ef6
dianajlailaty <66261096+dianajlailaty@users.noreply.github.com>
2021-08-26 15:10:34 +0200

    Update prophet.py

--- prophet/prophet/prophet.py
+++ prophet/prophet/prophet.py
@@ -27,7 +27,7 @@ import json
 import os
 import math
 
-directory_path = "morphemic_project/morphemic_integration/forecasting_prophet/"
+directory_path = "morphemic_project/forecasting_prophet/prophet/"
 
 def train(metric):
     #loading the dataset
